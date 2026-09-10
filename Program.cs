b695d636a95b93dc9a55c55272f1a3f3c8e6341b
Rasim Kryeziu <190514368+Agent-CB@users.noreply.github.com>
2026-09-10 18:17:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace multiplikationstabellen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multiplikationstabellen");
+
+            Console.WriteLine("Ange ett tal:");
+            int tal = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i<= 100; i++)
+            {
+                Console.WriteLine(i);
+
+                for (int j = 1; j <= 100; j++)
+                {
+
+                    Console.WriteLine($"{tal} * {j} = {tal * j}");
+                }
+
+            }
+        }
+    }
+}
